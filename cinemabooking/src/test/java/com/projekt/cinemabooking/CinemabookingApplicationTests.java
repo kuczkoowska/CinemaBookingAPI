87605f4f8d151583d10e1b929efd34be08cc3583
kuczkoowska <jagoda.kucz@wp.pl>
2025-12-13 18:13:22 +0100

    init

---
+++ cinemabooking/src/test/java/com/projekt/cinemabooking/CinemabookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.cinemabooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemabookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
